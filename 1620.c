a104abbe53a5ff7451737db6a9bf2414a90f0dad
minseokle <0311ben@naver.com>
2022-07-09 00:20:42 +0900

    update2022/7/8

--- 1620.c
+++ 1620.c
@@ -12,9 +12,29 @@ int main()
 	{
 		name[i] = name[i - 1] + 21;
 	}
+	int* seq = (int*)malloc(sizeof(int) * n);
+	int index[26] = { 0, };
 	for (int i = 0; i < n; i++)
 	{
 		scanf_s("%s", name[i], 20);
+		int al;
+		if (name[i][0] <= 'z' && name[i][0] >= 'a')
+		{
+			al = name[i][0] - 'a';
+		}
+		if (name[i][0] <= 'Z' && name[i][0] >= 'A')
+		{
+			al = name[i][0] - 'A';
+		}
+		for (int j = al+1; j < 26; j++)
+		{
+			index[j]++;
+		}
+		for (int j = n - 1; j >= index[al + 1]; j--)
+		{
+			seq[j] = seq[j - 1];
+		}
+		seq[index[al + 1] - 1] = i;
 	}
 	int** asw = (int**)malloc(sizeof(int*) * m);
 	asw[0] = (int*)malloc(sizeof(int) * m * 2);
@@ -41,19 +61,28 @@ int main()
 			asw[i][0] = 1;
 			int num = -1;
 			int j = 0;
+			int al;
+			if (qu[0] <= 'z' && qu[0] >= 'a')
+			{
+				al = qu[0] - 'a';
+			}
+			if (qu[0] <= 'Z' && qu[0] >= 'A')
+			{
+				al = qu[0] - 'A';
+			}
 			while (num == -1)
 			{
 				int k;
-				for (k = 0; qu[k] != NULL || name[j][k] != NULL; k++)
+				for (k = 0; qu[k] != NULL || name[seq[index[al] + j]][k] != NULL; k++)
 				{
-					if (qu[k] != name[j][k])
+					if (qu[k] != name[seq[index[al] + j]][k])
 					{
 						break;
 					}
 				}
 				if (qu[k] == NULL)
 				{
-					num = j;
+					num = seq[index[al] + j];
 				}
 				j++;
 			}
@@ -76,5 +105,6 @@ int main()
 	free(name);
 	free(asw[0]);
 	free(asw);
+	free(seq);
 	return 0;
 }
