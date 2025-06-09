2fc7b9cd40a04275f7b635f1e88a80d9b380ca43
Lee Min Seok <0311ben@naver.com>
2025-06-09 13:26:16 +0900

    Fix input handling and power calculation

--- 1074.c
+++ 1074.c
@@ -8,9 +8,9 @@ int squ(int a, int b);
 
 int main_1074()
 {
-	int n, r, c;
-	int arr1[2][2] = { 0,1,2,3 };
-	scanf_s("%d %d %d", &n, &r, &c);
+        int n, r, c;
+        int arr1[2][2] = { 0,1,2,3 };
+        scanf("%d %d %d", &n, &r, &c);
 	int size = 1;
 	size = squ(2, n);
 	int num = 0;
@@ -37,14 +37,10 @@ int sq(int a, int b)
 }
 int squ(int a, int b)
 {
-	int result = a;
-	if (b == 0)
-	{
-		return 1;
-	}
-	for (int i = 1; i < b; i++)
-	{
-		result *= result;
-	}
-	return result;
+        int result = 1;
+        for (int i = 0; i < b; i++)
+        {
+                result *= a;
+        }
+        return result;
 }
